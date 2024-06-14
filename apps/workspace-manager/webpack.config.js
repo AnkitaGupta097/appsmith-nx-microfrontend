f038b3deb20df71428bd3749687fcf92a071c57d
JaiZemoso <jai.jayanthilal@zemosolabs.com>
2024-06-14 12:39:14 +0530

    Integrating Workspace manager MF into NX MonoRepo

--- apps/workspace-manager/webpack.config.js
+++ apps/workspace-manager/webpack.config.js
@@ -1,30 +1,9 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
-const { withModuleFederation } = require('@nx/react/module-federation');
-const moduleFederationConfig =require('./module-federation.config');
-const { FederatedTypesPlugin } = require('@module-federation/typescript');
 
 // Nx plugins for webpack.
-module.exports = composePlugins(
-  withNx(),
-  withReact(),
-  withModuleFederation({...moduleFederationConfig}),
-  (config) => {
-    // Update the webpack config as needed here.
-    // e.g. `config.plugins.push(new MyPlugin())`
-    config.plugins.push(
-      new FederatedTypesPlugin({
-        federationConfig : {
-          ...moduleFederationConfig,
-          filename: 'remoteEntry.js'
-        }
-      })
-    )
-
-    config.devServer = {
-      ...config.devServer,
-      static: config.output.path
-    }
-    return config;
-  }
-);
+module.exports = composePlugins(withNx(), withReact(), (config) => {
+  // Update the webpack config as needed here.
+  // e.g. `config.plugins.push(new MyPlugin())`
+  return config;
+});
